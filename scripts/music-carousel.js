0140fcdbe79d56f7ac5dd48adca89f0790d2fa7c
Alexander-Kourjanski <sashakrj@gmail.com>
2024-09-15 16:50:51 -0700

    Updated Footer

--- scripts/music-carousel.js
+++ scripts/music-carousel.js
@@ -1,7 +1,8 @@
 const carousel = document.getElementById('musicCarousel');
 const nextBtn = document.getElementById('nextBtn');
 const prevBtn = document.getElementById('prevBtn');
-
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
 const scrollAmount = 550;
 
@@ -12,3 +13,15 @@ nextBtn.addEventListener('click', () => {
 prevBtn.addEventListener('click', () => {
     carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
 });
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
